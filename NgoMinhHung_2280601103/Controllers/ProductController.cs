c85e148277338bd5db23ad8b22281b8072b0d731
Gnuhit <ngohung9724@gmail.com>
2025-03-12 18:50:03 +0700

    Add

--- NgoMinhHung_2280601103/Controllers/ProductController.cs
+++ NgoMinhHung_2280601103/Controllers/ProductController.cs
@@ -2,93 +2,146 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 using NgoMinhHung_2280601103.Models;
 using NgoMinhHung_2280601103.Repository;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
 
-namespace NgoMinhHung_2280601103.Controllers
+public class ProductController : Controller
 {
-    public class ProductController : Controller
+    private readonly IProductRepository _productRepository;
+    private readonly ICategoryRepository _categoryRepository;
+
+    public ProductController(IProductRepository productRepository, ICategoryRepository categoryRepository)
     {
-        private readonly IProductRepository _productRepository;
-        private readonly ICategoryRepository _categoryRepository;
-        public ProductController(IProductRepository productRepository,
-       ICategoryRepository categoryRepository)
-        {
-            _productRepository = productRepository;
-            _categoryRepository = categoryRepository;
-        }
-        public async Task<IActionResult> Index()
-        {
-            var products = await _productRepository.GetAllAsync();
-            return View(products);
-        }
-        public async Task<IActionResult> AddAsync()
-        {
-            var categories = await _categoryRepository.GetAllAsync();
-            ViewBag.Categories = new SelectList(categories, "Id", "Name");
-            return View();
-        }
+        _productRepository = productRepository;
+        _categoryRepository = categoryRepository;
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> Add(Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                await _productRepository.AddAsync(product);
-                return RedirectToAction(nameof(Index));
-            }
+    public async Task<IActionResult> Index()
+    {
+        var products = await _productRepository.GetAllAsync();
+        return View(products);
+    }
 
-            var categories = await _categoryRepository.GetAllAsync();
-            ViewBag.Categories = new SelectList(categories, "Id", "Name");
-            return View(product);
-        }
-        public async Task<IActionResult> Display(int id)
+    public async Task<IActionResult> Add()
+    {
+        var categories = await _categoryRepository.GetAllAsync();
+        ViewBag.Categories = new SelectList(categories, "Id", "Name");
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add(Product product, IFormFile imageUrl, List<IFormFile> imageUrls)
+    {
+        if (ModelState.IsValid)
         {
-            var product = await _productRepository.GetByIdAsync(id);
-            if (product == null)
+            if (imageUrl != null)
             {
-                return NotFound();
+                product.ImageUrl = await SaveImage(imageUrl);
             }
-            return View(product);
-        }
-        public async Task<IActionResult> Update(int id)
-        {
-            var product = await _productRepository.GetByIdAsync(id);
-            if (product == null)
+            if (imageUrls != null)
             {
-                return NotFound();
+                product.Images = new List<ProductImage>();
+                foreach (var file in imageUrls)
+                {
+                    var image = new ProductImage
+                    {
+                        Url = await SaveImage(file),
+                        ProductId = product.Id
+                    };
+                    product.Images.Add(image);
+                }
             }
-            var categories = await _categoryRepository.GetAllAsync();
-            ViewBag.Categories = new SelectList(categories, "Id", "Name",
-            product.CategoryId);
-            return View(product);
-        }
-        [HttpPost]
-        public async Task<IActionResult> Update(int id, Product product)
-        {
-            if (id != product.Id)
-            {
-                return NotFound();
-            }
-            if (ModelState.IsValid)
-            {
-                await _productRepository.UpdateAsync(product);
-                return RedirectToAction(nameof(Index));
-            }
-            return View(product);
-        }
-        public async Task<IActionResult> Delete(int id)
-        {
-            var product = await _productRepository.GetByIdAsync(id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-            return View(product);
-        }
-        [HttpPost, ActionName("Delete")]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            await _productRepository.DeleteAsync(id);
+            await _productRepository.AddAsync(product);
             return RedirectToAction(nameof(Index));
         }
+
+        var categories = await _categoryRepository.GetAllAsync();
+        ViewBag.Categories = new SelectList(categories, "Id", "Name");
+        return View(product);
+    }
+
+    public async Task<IActionResult> Display(int id)
+    {
+        var product = await _productRepository.GetByIdAsync(id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+        return View(product);
+    }
+
+    public async Task<IActionResult> Update(int id)
+    {
+        var product = await _productRepository.GetByIdAsync(id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+        var categories = await _categoryRepository.GetAllAsync();
+        ViewBag.Categories = new SelectList(categories, "Id", "Name", product.CategoryId);
+        return View(product);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Update(int id, Product product, IFormFile imageUrl, List<IFormFile> imageUrls)
+    {
+        if (id != product.Id)
+        {
+            return NotFound();
+        }
+        if (ModelState.IsValid)
+        {
+            if (imageUrl != null)
+            {
+                product.ImageUrl = await SaveImage(imageUrl);
+            }
+            if (imageUrls != null)
+            {
+                product.Images = new List<ProductImage>();
+                foreach (var file in imageUrls)
+                {
+                    var image = new ProductImage
+                    {
+                        Url = await SaveImage(file),
+                        ProductId = product.Id
+                    };
+                    product.Images.Add(image);
+                }
+            }
+            await _productRepository.UpdateAsync(product);
+            return RedirectToAction(nameof(Index));
+        }
+        var categories = await _categoryRepository.GetAllAsync();
+        ViewBag.Categories = new SelectList(categories, "Id", "Name", product.CategoryId);
+        return View(product);
+    }
+
+    public async Task<IActionResult> Delete(int id)
+    {
+        var product = await _productRepository.GetByIdAsync(id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+        return View(product);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        await _productRepository.DeleteAsync(id);
+        return RedirectToAction(nameof(Index));
+    }
+
+    private async Task<string> SaveImage(IFormFile image)
+    {
+        var savePath = Path.Combine("wwwroot/images", image.FileName);
+        using (var fileStream = new FileStream(savePath, FileMode.Create))
+        {
+            await image.CopyToAsync(fileStream);
+        }
+        return "/images/" + image.FileName;
     }
 }
--- NgoMinhHung_2280601103/Repository/EFProductRepository.cs
+++ NgoMinhHung_2280601103/Repository/EFProductRepository.cs
@@ -12,11 +12,11 @@ namespace NgoMinhHung_2280601103.Repository
         }
         public async Task<IEnumerable<Product>> GetAllAsync()
         {
-            return await _context.Products.ToListAsync();
+            return await _context.Products.Include(p => p.Category).ToListAsync();
         }
         public async Task<Product> GetByIdAsync(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _context.Products.Include(p => p.Category).FirstOrDefaultAsync(p => p.Id == id);
         }
         public async Task AddAsync(Product product)
         {
@@ -36,3 +36,4 @@ namespace NgoMinhHung_2280601103.Repository
         }
     }
 }
+
